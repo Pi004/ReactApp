753383121c3d748c216cd9cdcde330dd7d30ba74
Pi004 <shrivastawpiyush04@gmail.com>
2024-10-23 02:06:22 +0530

    Basic Commit for folder functionality

---
+++ ReactApp1.Server/Controllers/FolderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactApp1.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FolderController : ControllerBase
+	{
+		// GET: api/<FolderController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<FolderController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<FolderController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<FolderController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<FolderController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ ReactApp1.Server/DBModels/Folders/FolderDAO.cs
@@ -0,0 +1,16 @@
+﻿using ReactApp1.Server.DBModels.Context;
+using ReactApp1.Server.Helper;
+
+namespace ReactApp1.Server.DBModels.Folders
+{
+	public interface IFolder
+	{
+
+	}
+	public class FolderDAO : IFolder
+	{
+		public readonly new ApplicationDBFactory _dbcontext;
+		public readonly IHelper _helper;
+
+	}
+}
